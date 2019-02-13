ee6f9d642281b31b090a876f93ed7e765a037644
Vesper <no-reply>
2019-02-13 10:11:24 -0500

    updated test code

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # RV_QAtest
+
+RV_QAtest demonstrates my knowledge of QA testing for Red Ventures. 
+
+## Tests Run
+
+**Website Tested:** https://www.creditcards.com/
+**Pre-Condition:** Access to Chrome Browser
+
+**Test Case:** Footer Link Functionality
+**Description/Why:** The footer contains the "fine print" of a website and important links to answer more in depth questions a user may have and provide more specific information on a topic. If the footer links do not function as expected, the business may lose potential new customers and opportunities to resolve issues with clients to build a positive user experience. 
+**Expected Result:** The "Contact Us" link should reroute to the "Contact Us" page
+**Status:** Pass
+
+**Test Case:** Navbar Functionality
+**Description/Why:** The Navbar is where users look to navigate through an entire site and find what they are searching for quickly and efficiently. If the Navbar links do not function as expected, the business will lose possible conversions and returning customers. 
+**Expected Result:** The Navbar link should have a dropdown menu with links that will reroute to the specified page upon click
+**Status:** Pass
+
+**Test Case:** Image Displayed
+**Description/Why:** The credit card image stands out on the page as a user scrolls and helps users identify the product they are being marketed. Users know that the section including this image will have a link or important information regarding the product that they are interested in. 
+**Expected Result:** The credit card image should appear upon page load
+**Status:** Pass
+
+**Test Case:** Style Test
+**Description/Why:** The business has marketing and branding standards that must be followed to maintain a reputable and trusted presence. If styles are not as expected, the consumer may view the site as an untrustworthy source if, for example, headline styles do not match each other or other body text.
+**Expected Result:** The headline should have and appear in the correct font-family
+**Status:** Pass
+
+## Technologies
+
+selenium-webdriver
+mocha
+chai
+
+## Installation
+
+```bash
+npm install
+```
+
+## Run
+
+```bash
+npm run test
+```
---
+++ test.js
@@ -0,0 +1,82 @@
+// Dependencies
+require('chromedriver');
+var {
+    Builder,
+    By
+} = require('selenium-webdriver')
+var chai = require('chai');
+var expect = chai.expect;
+
+// Variable to call our browser during our tests
+var driver
+
+// Before each async function, driver calls the web browser we will be using to run our tests
+beforeEach(async function () {
+    driver = await new Builder().forBrowser('chrome').build();
+});
+
+// After each async function is run, the web broswer will be closed signaling the end of that specific test
+afterEach(async function () {
+    await driver.quit();
+});
+
+// Test 1: Test 'Contact Us' footer link 
+describe('Test 1: Contact Us Footer', function () {
+
+    it('Should reroute from home page to "Contact Us" page on click', async function () {
+        await driver.get('https://www.creditcards.com');
+
+        // Finds the "Contact Us" link element by xpath and clicks
+        var contactUs = await driver.findElement(By.xpath('//body//footer/div[2]/div[2]/div[2]/div[3]/ul/li[5]/a'));
+        await contactUs.click();
+
+        // We ask the browser to retrieve the title on the "Contact Us" page and we expect it to match the title we specified 
+        var contactTitle = await driver.getTitle();
+        await expect(contactTitle).to.equal('CreditCards.com - Contact Us');
+    });
+});
+
+// Test 2: Test navbar "Card Category" dropdown menu "Rewards" link
+describe('Test 2: Card Category', function () {
+
+    it('Should show dropdown menu and have a working "Rewards" link under Card Category that reroutes to the Rewards page', async function () {
+        await driver.get('https://www.creditcards.com');
+
+        // Finds "Card Category" in the navbar, clicks, then finds the link labeled "Rewards" and clicks
+        var navLink1 = await driver.findElement(By.linkText('Card Category'));
+        await navLink1.click();
+        await driver.findElement(By.linkText('Rewards')).click();
+
+        // Browser retrieves the title on the "Rewards" page and matches it to what we expect the title to be
+        var headerTitle = await driver.getTitle();
+        await expect(headerTitle).to.equal('Best Rewards Credit Cards of 2019: Top 10 Offers - CreditCards.com');
+    });
+});
+
+// Test 3: Show that the red Bank of America credit card image is present upon page load
+describe('Test 3: Credit Card Image Present', function () {
+
+    it('Should display that the red Bank of America credit card image is present upon page load', async function () {
+        await driver.get('https://www.creditcards.com');
+
+        // Finds the red credit card image element by its class name then looks at that classe's tag name
+        await driver.findElement(By.className('home-page-issuer__image-wrapper'));
+        var creditCardImg = await driver.findElement(By.tagName('img'));
+
+        // isDisplayed() is a boolean method which will tell us if the image is displayed or not
+        creditCardImg.isDisplayed();
+    });
+});
+
+// Test 4: Make sure that the headline text is using the correct font
+describe('Test 4: Headline Text', function () {
+
+    it('Should have the "Find the Perfect Credit Card for You" headline font family as "sofiaproBold, Arial, sans-serif"', async function () {
+        await driver.get('https://www.creditcards.com');
+
+        // Finds the headline by its class name then retrieves the CSS value for its font-family
+        // We expect the font family found to match what we have specified
+        var headerFont = await driver.findElement(By.className('homePage-hero__headline')).getCssValue("font-family");
+        await expect(headerFont).to.equal('sofiaproBold, Arial, sans-serif');
+    });
+});
